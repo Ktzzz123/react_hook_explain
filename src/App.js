8d78cc14a6c68526ef11ddd209989183ede63b7f
Jelly <18521491@gm.uit.edu.vn>
2022-07-14 11:14:31 +0700

    first in EuseEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import EuseEffect from './components/EuseEffect';
 import EuseReducer from './components/EuseReducer';
 import EuseState from './components/EuseState';
 
 
 function App() {
- 
+  
   return (
     <div className="App">
       <header className="App-header">
 
         {/* <EuseState/> */}
         {/* <EuseReducer/> */}
+        <EuseEffect />
       </header>
     </div>
   );
--- src/components/EuseEffect.jsx
+++ src/components/EuseEffect.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
 
-function EuseEffect(props) {
+
+
+function EuseEffect() {
+  const [data,setData] = useState("");
+
+   
+    let firstData = "render 1 time";
+
+    useEffect(()=>{
+        axios
+        .get("https://jsonplaceholder.typicode.com/comments")
+        .then(function (response) {
+            setData(response.data[0].email)
+        });
+        console.log('API was called')
+    },[])
+    {console.log(firstData)}
     return (
         <div>
-                
+            <h1>{firstData}</h1>
 
-     
+            {data}
         </div>
     );
 }
