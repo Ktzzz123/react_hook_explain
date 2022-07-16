8c6021815e420e23f6a9ef136d6cf000f11637c6
Jelly <18521491@gm.uit.edu.vn>
2022-07-16 09:43:48 +0700

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,25 +4,38 @@ import EuseReducer from './components/EuseReducer';
 import EuseState from './components/EuseState';
 import EuseRef from './components/EuseRef';
 import EuseLayoutEffect from './components/EuseLayoutEffect'
-import Home from './components/Home';
+
 import EuseImperativeHangdle from './components/EuseImperativeHandle'
+import React from 'react'
 
 import {Routes,Route} from "react-router-dom";
 import EuseContext from './components/EuseContext';
 import EuseMemo from './components/EuseMemo';
 import EuseCallback from './components/EuseCallback';
+import Home from './components/Home.jsx'
 
 function App() {
   
   return (
-    <div className="App">
-      {/* <Home/> */}
+    <div>
+      <Home/>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/useEffect" element = {<EuseEffect/>}/>
+        <Route path = "/useReducer" element = {<EuseReducer/>}/>
+        <Route path = "/useState" element = {<EuseState/>}/>
+        <Route path = "/useRef" element = {<EuseRef/>}/>
+        <Route path = "/useLayoutEffect" element = {<EuseLayoutEffect/>}/>
+        <Route path = "/useContext" element = {<EuseContext/>}/>
+        <Route path = "/useMemo" element = {<EuseMemo/>}/>
+        <Route path = "/useCallback" element = {<EuseCallback/>}/>
+        <Route path='/useImperativeHangdle' element ={<EuseImperativeHangdle/>}/>
+      </Routes>
       
-
     {/* <EuseImperativeHangdle/> */}
     {/* <EuseContext/> */}
     {/* <EuseMemo/> */}
-    <EuseCallback/>
+    {/* <EuseCallback/> */}
 
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,17 +5,19 @@ function Home () {
         return (
             <div>
                 <div>
-                
-                <button to ='/useState'>useState</button>
-                <button to = '/useReducer'>useReducer</button>
-                <button to = '/useEffect'>useEffect</button>
-                <button to = '/useLayoutEffect'>useLayoutEffect</button>
-                <button to = '/useRef'>useRef</button>
+                <button onClick={()=>window.location.href='/'} >Home</button>
+                <button onClick={()=>window.location.href ='/useState'}>useState</button>
+                <button onClick={()=>window.location.href = '/useReducer'}>useReducer</button>
+                <button onClick={()=>window.location.href = '/useEffect'}>useEffect</button>
+                <button onClick={()=>window.location.href = '/useLayoutEffect'}>useLayoutEffect</button>
+                <button onClick={()=>window.location.href = '/useRef'}>useRef</button>
+                <button onClick={()=>window.location.href='/useContext'}>useContext</button>
+                <button onClick={()=>window.location.href='/useMemo'} >useMemo</button>
+                <button onClick={()=>window.location.href='/useCallback'}>useCallback</button>
+                <button onClick={()=>window.location.href='/useImperativeHangdle'}>useImperativeHangdle</button>
                 </div>
-                
             </div>
         )
-
 }
 
 export default Home;
