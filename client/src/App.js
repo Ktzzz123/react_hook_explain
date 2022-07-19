3ab54247f7c063c963126638515af8054402766b
Jelly <Tieukieutien2000@gmail.com>
2022-07-19 16:01:33 +0700

    finish basic tutorial for socketIO

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import io from 'socket.io-client'
 import './App.css';
+import {useEffect,useState} from 'react'
 
+
+const socket = io.connect("http://localhost:3001")
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageRecived,setMessageRecived]=useState("")
+
+
+  const sendMessage = ()=>{
+    socket.emit("send_message", {message})
+  }
+
+  useEffect(()=>{
+    socket.on("receive_message",(data)=>{
+      setMessageRecived(data.message)
+    });
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+        <input onChange={(e)=>{
+          setMessage(e.target.value)
+        }} placeholder="Message..."/>
+        <button onClick={sendMessage}>Send Message</button>
+        <h1>Message:</h1>
+        {messageRecived}
     </div>
   );
 }
